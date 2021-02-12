ca22b6462e2095932843a270fb59ee71bb59d5df
Jeon Jun Ik <jji3379@naver.com>
2021-02-13 00:58:29 +0900

    First_Setting

---
+++ Booking/src/main/java/com/acorn5/booking/HomeController.java
@@ -0,0 +1,26 @@
+package com.acorn5.booking;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home.do")
+	public String home() {
+		
+		return "home";
+	}
+	
+}
